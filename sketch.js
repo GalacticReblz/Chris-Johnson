03f5c4ef3cbbda836069584830acaf3de6a965f6
GalacticReblz <100540735+GalacticReblz@users.noreply.github.com>
2022-06-20 17:20:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var student
+
+function setup() {
+  createCanvas(400,400);
+
+  student = new Student("Chris Johnson", 25, "12th");
+  student.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,16 @@
+
+class Student
+{
+   constructor(name,age,grade){
+    this.name = name
+    this.age = age
+    this.grade = grade
+   }
+
+   display(){
+       console.log(this.name + this.age + this.grade)
+   }
+}
+
+
+
